18134e44d5c26b9dd1555e0db8114db3b9bb95e9
Gurrman375 <james.gurr375@gmail.com>
2024-01-07 19:32:45 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,77 +1,147 @@
-function probateCalculator(total){
-    let probateNumber = 0;
-    let displayMessage;
-    let identity;
-    if(total == 0){
-        window.alert("Invaild Range");
+const displayMessage = "Invalid Number: \nEnter a number between 100,000.00 - 25,000,000.00";
+        const percent = [75000, 90000, 16000, 3000, 4000];
+        function probateCalculator(total){
+            let probateNumber = 0;
+            let displayMessage;
+            let identity;
+            if(total == 0){
+                window.alert("Invalid Character: \nEnter a number between 100,000.00 - 25,000,000.00");
+            }
+            else{
+                if(total > 25000000){
+                    window.alert(displayMessage);
+                }
+                else if((25000000 >= total) && (total > 10000000)){ // 10
+                    probateNumber = percent[4]+percent[3]+percent[2]+percent[1];
+                    total -= 10000000;
+                    total *= 0.005;
+                    probateNumber += total;
+                    identity = 4;
+                }
+                else if((10000000 >= total) && (total > 1000000)){
+                    probateNumber = percent[4]+percent[3]+percent[2];
+                    total -= 1000000;
+                    total *= 0.01;
+                    probateNumber += total;
+                    identity = 3;
+                }
+                else if((1000000 >= total) && (total > 200000)){
+                    probateNumber = percent[4] + percent[3];
+                    total -= 200000;
+                    total *= 0.02;
+                    probateNumber += total;
+                    identity = 2;
+                }
+                else if((200000 >= total) && (total >= 100000)){
+                    probateNumber = percent[4];
+                    total -= 100000;
+                    total *= 0.03;
+                    probateNumber += total;
+                    identity = 1;
+                }
+                else{
+                    window.alert(displayMessage);
+                }
+                const labels = ["4% of first $100,000", "3% of the next $100,000", "2% of the next $800,000", "1% of the next $9,000,000", "0.5% of the next $15,000,000"];
+                let data;
+                switch(identity){
+                    case 1:
+                        document.getElementById("4perc").innerHTML = "$" + numberWithCommas(percent[4].toFixed(2));
+                        document.getElementById("3perc").innerHTML = "$" + numberWithCommas(total.toFixed(2));
+                        document.getElementById("2perc").innerHTML = "$" + 0.00;
+                        document.getElementById("1perc").innerHTML = "$" + 0.00;
+                        document.getElementById("hperc").innerHTML = "$" + 0.00;
+                        data = [percent[4].toFixed(2), total.toFixed(2)];
+                        break;
+                    case 2:
+                        document.getElementById("4perc").innerHTML = "$" + numberWithCommas(percent[4].toFixed(2));
+                        document.getElementById("3perc").innerHTML = "$" + numberWithCommas(percent[3].toFixed(2));
+                        document.getElementById("2perc").innerHTML = "$" + numberWithCommas(total.toFixed(2));
+                        document.getElementById("1perc").innerHTML = "$" + 0.00;
+                        document.getElementById("hperc").innerHTML = "$" + 0.00;
+                        data = [percent[4].toFixed(2), percent[3].toFixed(2), total.toFixed(2)];
+                        break;
+                    case 3:
+                        document.getElementById("4perc").innerHTML = "$" + percent[4].toFixed(2);
+                        document.getElementById("3perc").innerHTML = "$" + percent[3].toFixed(2);
+                        document.getElementById("2perc").innerHTML = "$" + percent[2].toFixed(2);
+                        document.getElementById("1perc").innerHTML = "$" + total.toFixed(2);
+                        document.getElementById("hperc").innerHTML = "$" + 0.00;
+                        data = [percent[4].toFixed(2), percent[3].toFixed(2), percent[2].toFixed(2), total.toFixed(2)];
+                        break;
+                    case 4:
+                        document.getElementById("4perc").innerHTML = "$" + numberWithCommas(percent[4].toFixed(2));
+                        document.getElementById("3perc").innerHTML = "$" + numberWithCommas(percent[3].toFixed(2));
+                        document.getElementById("2perc").innerHTML = "$" + numberWithCommas(percent[2].toFixed(2));
+                        document.getElementById("1perc").innerHTML = "$" + numberWithCommas(percent[1].toFixed(2));
+                        document.getElementById("hperc").innerHTML = "$" + numberWithCommas(total.toFixed(2));
+                        data = [percent[4].toFixed(2), percent[3].toFixed(2), percent[2].toFixed(2), percent[1].toFixed(2), total.toFixed(2)];
+                        break;
+                }
+                document.getElementById("A").innerHTML = "$" + numberWithCommas(probateNumber.toFixed(2));
+                document.getElementById("E").innerHTML = "$" + numberWithCommas(probateNumber.toFixed(2));
+                document.getElementById("AE").innerHTML = "$" + numberWithCommas((probateNumber + probateNumber).toFixed(2));
+                if(document.getElementById("canvas") != null){
+                    createChart(labels, data);
+                }
+            }
+        }
+    // modify according to site
+    function createChart(labels, data){
+        if(!labels){
+            const labels = ["4% of first $100,000", "3% of the next $100,000", "2% of the next $800,000", "1% of the next $9,000,000", "0.5% of the next $15,000,000"];
+        }
+        document.querySelector("body > div:nth-child(11) > canvas").remove();
+        if(document.querySelector("body > div:nth-child(11)").childNodes[0]){
+            document.querySelector("body > div:nth-child(11)").childNodes[0].remove();
+        }
+        let canvas = document.createElement('canvas');
+        canvas.id = "canvas";
+        document.querySelector("body > div:nth-child(11)").appendChild(canvas);
+        const formatter = new Intl.NumberFormat('en-US', {
+            style: 'currency',
+            currency: 'USD',
+            minimumFractionDigits: 2
+        });
+
+        const datad = {
+            labels: labels,
+            datasets: [{
+                label: 'Weekly Sales',
+                data: data,
+                backgroundColor: ["#FBE2CD", "#F7C79D", "#F4AF74", "#F09445", "#ED7916"]
+            }]
+        };
+        const config = {
+            type: 'pie',
+            data: datad,
+            options: {
+                plugins:{
+                    tooltip:{
+                        callbacks: {
+                            label: (context) => {
+                                const value = context.parsed;
+                                let label;
+                                if(value !== null){
+                                    label = "$" + numberWithCommas(value.toFixed(2));
+                                }
+                                return label;
+                            }
+                        }
+                    }
+                }
+                }
+        };
+        const myChart = new Chart(
+            document.getElementById('canvas'),
+            config
+        );
     }
-    else{
-        if(total > 25000000){
-            displayMessage = "Invalid Number: \nEnter a number between 100,001.00 - 25,000,000.00";
-            window.alert(displayMessage);
-        }
-        else if((25000000 >= total) && (total > 10000000)){ // 10
-            probateNumber = 113000; 
-            total -= 10000000; 
-            total *= 0.005;
-            probateNumber += total;
-            identity = 4;
-        }
-        else if((10000000 >= total) && (total > 1000000)){
-            probateNumber = 23000;
-            total -= 1000000;
-            total *= 0.01;
-            probateNumber += total;
-            identity = 3;
-        }
-        else if((1000000 >= total) && (total > 200000)){
-            probateNumber = 7000;
-            total -= 200000;
-            total *= 0.02;
-            probateNumber += total;
-            identity = 2;
-        }
-        else if((200000 >= total) && (total > 100000)){
-            probateNumber = 4000;
-            total -= 100000;
-            total *= 0.03;
-            probateNumber += total;
-            identity = 1;
-        }
-        else{
-            displayMessage = "Invalid Number: \nEnter a number between 100,001.00 - 25,000,000.00";
-            window.alert(displayMessage);
-        }
-        switch(identity){
-            case 1:
-                document.getElementById("4perc").innerHTML = "$" + 4000.00;
-                document.getElementById("3perc").innerHTML = "$" + total.toFixed(2);
-                document.getElementById("2perc").innerHTML = "$" + 0.00;
-                document.getElementById("1perc").innerHTML = "$" + 0.00;
-                document.getElementById("hperc").innerHTML = "$" + 0.00;
-                    break;
-            case 2:
-                document.getElementById("4perc").innerHTML = "$" + 4000.00;
-                document.getElementById("3perc").innerHTML = "$" + 3000.00;
-                document.getElementById("2perc").innerHTML = "$" + total.toFixed(2);
-                document.getElementById("1perc").innerHTML = "$" + 0.00;
-                document.getElementById("hperc").innerHTML = "$" + 0.00;
-                break;
-            case 3:
-                document.getElementById("4perc").innerHTML = "$" + 4000.00;
-                document.getElementById("3perc").innerHTML = "$" + 3000.00;
-                document.getElementById("2perc").innerHTML = "$" + 16000.00;
-                document.getElementById("1perc").innerHTML = "$" + total.toFixed(2);
-                document.getElementById("hperc").innerHTML = "$" + 0.00;
-                break;
-            case 4:
-                document.getElementById("4perc").innerHTML = "$" + 4000;
-                document.getElementById("3perc").innerHTML = "$" + 3000;
-                document.getElementById("2perc").innerHTML = "$" + 16000;
-                document.getElementById("1perc").innerHTML = "$" + 90000;
-                document.getElementById("hperc").innerHTML = "$" + total.toFixed(2);
-                break;
-        }
-            document.getElementById("test").innerHTML = "$" + probateNumber.toFixed(2);
+
+    /*
+    * Code from Aryan Beezadhur and Elias Zamaria
+    * https://stackoverflow.com/questions/2901102/how-to-format-a-number-with-commas-as-thousands-separators
+    */
+    function numberWithCommas(x) {
+        return x.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
     }
-}
